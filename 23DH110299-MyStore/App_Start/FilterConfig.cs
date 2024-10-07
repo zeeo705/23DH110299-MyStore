0aa50226f1da22a83e1cd2aa0ddcf468c5e8db68
SV <SV@PM19-PC09>
2024-10-07 11:41:18 +0700

    Add project files.

---
+++ 23DH110299-MyStore/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH110299_MyStore
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ 23DH110299-MyStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_23DH110299_MyStore.MvcApplication" Language="C#" %>
